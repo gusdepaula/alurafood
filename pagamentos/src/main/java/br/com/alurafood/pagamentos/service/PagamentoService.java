c7d137ee591250d08e015584e428051bcd2f68b9
Gustavo de Paula <gdepaula@gmail.com>
2026-05-11 16:16:14 -0300

    Comunicação sincrona

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -1,6 +1,7 @@
 package br.com.alurafood.pagamentos.service;
 
 import br.com.alurafood.pagamentos.dto.PagamentoDto;
+import br.com.alurafood.pagamentos.http.PedidoClient;
 import br.com.alurafood.pagamentos.model.Pagamento;
 import br.com.alurafood.pagamentos.repository.PagamentoRepository;
 import javax.persistence.EntityNotFoundException;
@@ -12,10 +13,15 @@ import br.com.alurafood.pagamentos.model.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 @Service
 public class PagamentoService {
     @Autowired
     private PagamentoRepository repository;
+
+    @Autowired
+    private PedidoClient pedido;
 
     @Autowired
     private ModelMapper modelMapper;
@@ -51,6 +57,18 @@ public class PagamentoService {
 
     public void excluirPagamento(Long id) {
         repository.deleteById(id);
+    }
+
+    public void confirmarPagamento(Long id){
+        Optional<Pagamento> pagamento = repository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO);
+        repository.save(pagamento.get());
+        pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
 
 }
