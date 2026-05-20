b9572d936a3d5c8a371a7a6cce5a23f2bf59c3b3
Gustavo de Paula <gdepaula@gmail.com>
2026-05-20 14:41:25 -0300

    Transferindo representações

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/ampq/PagamentoAMPQConfiguration.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/ampq/PagamentoAMPQConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +19,6 @@ import java.util.function.Consumer;
 public class PagamentoAMPQConfiguration {
     @Bean
     public Queue criaFila(){
-        // return  new Queue("pagamento.concluido", false);
         return QueueBuilder.nonDurable("pagamento.concluido").build();
     }
 
@@ -29,5 +30,17 @@ public class PagamentoAMPQConfiguration {
     @Bean
     public ApplicationListener<ApplicationReadyEvent> inicializaAdmin(RabbitAdmin rabbitAdmin){
         return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        return  new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return  rabbitTemplate;
     }
 }
