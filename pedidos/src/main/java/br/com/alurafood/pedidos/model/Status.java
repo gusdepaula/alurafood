0c7ad1505f9162b8d75723be8f2c3f51120504e0
Gustavo de Paula <gdepaula@gmail.com>
2026-05-20 14:57:02 -0300

    Manipulando os dados recebidos

--- pedidos/src/main/java/br/com/alurafood/pedidos/model/Status.java
+++ pedidos/src/main/java/br/com/alurafood/pedidos/model/Status.java
@@ -2,11 +2,8 @@ package br.com.alurafood.pedidos.model;
 
 public enum Status {
     REALIZADO,
-    CANCELADO,
     PAGO,
-    NAO_AUTORIZADO,
-    CONFIRMADO,
-    PRONTO,
-    SAIU_PARA_ENTREGA,
-    ENTREGUE;
+    ENTREGUE,
+    CANCELADO
+
 }
